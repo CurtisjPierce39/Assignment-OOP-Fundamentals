51b87a360b03b44ad2011b831738b93b6bdb0925
Curtis J Pierce <Curtisjpierce39@gmail.com>
2024-10-23 07:50:28 -0700

    add intial description

--- City Infrastructure Management System(Task 1).py
+++ City Infrastructure Management System(Task 1).py
@@ -12,45 +12,52 @@ class Vehicle:
     def display_owners(self):
         print(f"Registration #: {self.reg_number}, Type: {self.type}, Owner: {self.owner}")
 
-vehicles = {}
 
-def register_vehicles(reg_num, type, owner):
-    if reg_num in vehicles:
-        print("Error!! Please try again!")
-        return
-    vehicles[reg_num] = Vehicle(reg_num, type, owner)
-    print(f"Vehicle '{reg_num}' now fully registered.")
+first_owner = Vehicle("abc-123", "car", "Bob Belcher")
+second_owner = Vehicle("def-456", "truck", "Linda Belcher")
 
-def update_owner(reg_num, new_owner):
-    if reg_num in vehicles:
-        vehicles[reg_num].update_owner(new_owner)
-        print(f"Registration number '{reg_num}' has a new owner!")
-    else:
-        print("No vehicle by that registration number.")
+first_owner.display_owners()
+second_owner.display_owners()
 
-def display_all_vehicles():
-    if not vehicles:
-        print("No vehicles to display!")
-    for vehicle in vehicles.values():
-        vehicle.display_owners()
+# vehicles = {}
 
-while True:
-    print("1. Register Vehicle \n2. Update Owner \n3. Display All Vehicles \n4. Quit")
-    choice = input("Please select an option: ")
-    try:
-        if choice == '1':
-            reg_num = input("Please enter registration number: ")
-            type = input("Please enter vehicle type: ")
-            owner = input("Please enter owner name: ")
-            register_vehicles(reg_num, type, owner)
-        elif choice == '2':
-            reg_num = input("Please enter registration number: ")
-            new_owner = input("Please enter new owner name: ")
-            update_owner(reg_num, new_owner)
-        elif choice == '3':
-            display_all_vehicles()
-        elif choice == '4':
-            print("Thank you. Come Again!")
-            break
-    except Exception as e:
-        print(f"An error has occurred: {e}")
+# def register_vehicles(reg_num, type, owner):
+#     if reg_num in vehicles:
+#         print("Error!! Please try again!")
+#         return
+#     vehicles[reg_num] = Vehicle(reg_num, type, owner)
+#     print(f"Vehicle '{reg_num}' now fully registered.")
+
+# def update_owner(reg_num, new_owner):
+#     if reg_num in vehicles:
+#         vehicles[reg_num].update_owner(new_owner)
+#         print(f"Registration number '{reg_num}' has a new owner!")
+#     else:
+#         print("No vehicle by that registration number.")
+
+# def display_all_vehicles():
+#     if not vehicles:
+#         print("No vehicles to display!")
+#     for vehicle in vehicles.values():
+#         vehicle.display_owners()
+
+# while True:
+#     print("1. Register Vehicle \n2. Update Owner \n3. Display All Vehicles \n4. Quit")
+#     choice = input("Please select an option: ")
+#     try:
+#         if choice == '1':
+#             reg_num = input("Please enter registration number: ")
+#             type = input("Please enter vehicle type: ")
+#             owner = input("Please enter owner name: ")
+#             register_vehicles(reg_num, type, owner)
+#         elif choice == '2':
+#             reg_num = input("Please enter registration number: ")
+#             new_owner = input("Please enter new owner name: ")
+#             update_owner(reg_num, new_owner)
+#         elif choice == '3':
+#             display_all_vehicles()
+#         elif choice == '4':
+#             print("Thank you. Come Again!")
+#             break
+#     except Exception as e:
+#         print(f"An error has occurred: {e}")
--- City Infrastructure Management Sysytem(Task 2).py
+++ City Infrastructure Management Sysytem(Task 2).py
@@ -5,9 +5,26 @@ class Event:
         self.name = name
         self.date = date
         self.participant_count = 0
+        # self.participants = []
         
     def add_participant(self):
         self.participant_count += 1
+        return self.participant_count
 
     def get_participant_count(self):
         return self.participant_count
+    
+    def display_count(self):
+        print(f"Event: {self.name}, Date: {self.date}, Number of participants: {self.participant_count}")
+        # print("Participants =" ,', '.join(self.participants))
+
+
+first_event = Event("Toy Drive", "10-31-2002")
+second_event = Event("Food Drive", "02-15-1986")
+
+toy_drive = first_event.add_participant()
+food_drive = second_event.add_participant()
+food_drive = second_event.add_participant()
+
+first_event.display_count()
+second_event.display_count()
